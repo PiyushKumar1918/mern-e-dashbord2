181fb04bbd241348afcaea2b8e6aa16dbbfa453a
piyush kumar <123872663+PiyushKumar1918@users.noreply.github.com>
2023-08-11 10:09:23 +0530

    plus

--- ecomm-frontend/src/App.js
+++ ecomm-frontend/src/App.js
@@ -1,10 +1,27 @@
-
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <h1>E-Dashbord</h1>
+      <BrowserRouter>
+    
+     <Nav />
+     <Routes>
+      <Route path="/" element={<h1>Product Listing Component</h1>}/>
+      <Route path="/add" element={<h1>Add Product</h1>}/>
+      <Route path="/update" element={<h1>Update Product</h1>}/>
+      <Route path="/logout" element={<h1>Logout Page</h1>}/>
+      <Route path="/profile" element={<h1>Profile</h1>}/>
+      <Route path="/signup" element={<SignUp />}/>
+
+     </Routes>
+     </BrowserRouter>
+     <Footer />
+
     </div>
   );
 }
