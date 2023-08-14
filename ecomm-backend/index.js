f9ffd6b753a08c56b2d9899aefda9d5e5d90bbf6
piyush kumar <123872663+PiyushKumar1918@users.noreply.github.com>
2023-08-14 15:04:28 +0530

    add

--- ecomm-backend/index.js
+++ ecomm-backend/index.js
@@ -1,13 +1,32 @@
-const express = require('express');
-require('./db/config');
-const User = require("./db/User")
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
 const app = express();
+
+app.use(cors());
 app.use(express.json());
-app.post("/register", async (req,resp)=>{
-    let user=new User(req.body);
-    let result =await user.save()
-    resp.send(result);
-})
 
+app.post("/register", async (req, resp) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password
+  resp.send(result);
+});
 
-app.listen(5000)
+app.post("/login", async (req, resp) => {
+  console.log(req.body);
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User Found" })
+    }
+  } else {
+    resp.send({ result: "No User Found" })
+  }
+});
+
+app.listen(5000);
--- ecomm-frontend/src/components/SignUp.js
+++ ecomm-frontend/src/components/SignUp.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {useNavigate} from 'react-router-dom'
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const collectData =()=>{
-    console.warn(name,email,password)
-  }
+
+  useEffect(()=>{
+      const auth = localStorage.getItem('user')
+      if(auth)
+      {
+          navigate('/')
+      }
+  })
+
+  const collectData = async () => {
+    console.warn(name, email, password);
+    let result = await fetch("http://localhost:5000/register", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    localStorage.setItem("user", JSON.stringify(result));
+      navigate('/')
+  };
   return (
     <div className="register">
       <h1>Register</h1>
